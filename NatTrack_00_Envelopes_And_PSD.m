467deebc9def6fcf8974cc4c6e892c955e15142d
srosorio <sosoriogaleano@mgh.harvard.edu>
2023-10-06 18:05:27 -0400

    Edited plotting sections in the code

--- NatTrack_00_Envelopes_And_PSD.m
+++ NatTrack_00_Envelopes_And_PSD.m
@@ -7,6 +7,7 @@ addpath('F:\Matlab\IEEG\Scripts\NSL');
 % create a directory to save data
 iEEG_dir = 'F:\Matlab\IEEG';
 data_dir = [iEEG_dir,filesep,'Data'];
+Fig_Dir  = [iEEG_dir,filesep,'FiguresCROSCORR'];
 
 % load audio file
 [y,fs] = audioread('F:\Matlab\IEEG\Stimulus\audio.wav');
@@ -49,11 +50,11 @@ for idx=1:length(music_segments)-1
     speechsignal  = resample(speech_segments{idx},tseg,16000);
     
     % v corresponds to the signals in the 128 frequencies between 180 and 7142 Hz
-    [music_v,~]      = wav2aud2(musicsignal,[5 8 -2 0]);
-    [speech_v,~]     = wav2aud2(speechsignal,[5 8 -2 0]);
+    [music_v_all(:,:,idx),freqs]      = wav2aud2(musicsignal,[5 8 -2 0]);
+    [speech_v_all(:,:,idx),~]     = wav2aud2(speechsignal,[5 8 -2 0]);
     
-    music_v  = detrend(music_v);
-    speech_v = detrend(speech_v);
+    music_v  = detrend(music_v_all(:,:,idx));
+    speech_v = detrend(speech_v_all(:,:,idx));
     
     % get the envelope by averaging across frequencies
     envelope_music(idx,:)  = mean(music_v');
@@ -66,22 +67,19 @@ save([data_dir,filesep,'envelopes_speech.mat'],'envelope_speech');
 
 %% Get PSDs
 
-% low pass filter the data to look for slow frequency peaks
-envelope_musicFILT  = lowpass(envelope_music',10,200,'Steepness',.95)';
-envelope_speechFILT = lowpass(envelope_speech',10,200,'Steepness',.95)';
-
 % get powers spectrum density for music
-[psd_music, frex_music] = pwelch(envelope_musicFILT',[],[],[],250);
+[psd_music, frex_music] = pwelch(envelope_music',2000,1000,[],250);
 % normalize spectrum
 psd_music = psd_music ./ sum(psd_music);
 
 % plot
-figure(1)
+figure(1), clf
 h1 = plot(frex_music,psd_music(:,1:6),'linew',1);
-xlim([0 12]); ylim([0 .3]);
+xlim([0 8]);
+ylim([0 .3]);
 xlabel('Frequency (Hz)'); ylabel('Normalized power');
 title('Music','FontWeight','normal');
-set(gca,'FontSize',20);
+set(gca,'FontSize',12);
 box off, hold on
 
 for idx=1:numel(h1)-1
@@ -90,20 +88,21 @@ end
 
 h2 = plot(frex_music,mean(psd_music(:,1:6),2),'linew',2); 
 legend({'Segment 1', 'Segment 2', 'Segment 3', 'Segment 4', 'Segment 5', 'Segment 6','Average'},'FontSize',14,'Box','off');
-% saveas(gca,'MusicStimuliPSD.tiff');
+saveas(gca,[Fig_Dir,filesep,'MusicStimuliPSD.tiff']);
 
 % now get the PSD for speech
-[psd_speech, frex_speech] = pwelch(envelope_speechFILT',[],[],[],250);
+[psd_speech, frex_speech] = pwelch(envelope_speech',2000,1000,[],250);
 % normalize spectrum
 psd_speech = psd_speech ./ sum(psd_speech);
 
 % plot
-figure(2)
-h1 = plot(frex_music,psd_speech,'linew',1);
-xlim([0 12]); ylim([0 .3]);
+figure(2), clf
+h1 = plot(frex_speech,psd_speech,'linew',1);
+xlim([0 12]); 
+ylim([0 .3]);
 xlabel('Frequency (Hz)'); ylabel('Normalized power');
 title('Speech','FontWeight','normal');
-set(gca,'FontSize',20);
+set(gca,'FontSize',12);
 box off, hold on
 
 for idx=1:numel(h1)
@@ -112,9 +111,11 @@ end
 
 plot(frex_speech,mean(psd_speech,2),'linew',2,'Color', h2.Color);
 legend({'Segment 1', 'Segment 2', 'Segment 3', 'Segment 4', 'Segment 5', 'Segment 6','Average'},'FontSize',14,'Box','off');
-% saveas(gca,'SpeechStimuliPSD.tiff');
+saveas(gca,[Fig_Dir,filesep,'SpeechStimuliPSD.tiff']);
+
 
 %% Plot raw acoustic signals
+close all
 colors  = [0.7176 0.2745 1.0000; ...
            0.9412 0.5804 0.3373; ...
            0.4667 0.6745 0.1882];   
@@ -122,75 +123,39 @@ colors  = [0.7176 0.2745 1.0000; ...
 % plot music and speech signals
 
 for idx=1:length(music_segments)-1
-    figure(3)
-    subplot(6,2,(idx*2)-1)
-    ph = plot(tseg,music_segments{idx}(:,1),'color',colors(1,:));
-    ylim([-1 1]); xlim([0 28.90]);
-    box off
-    if idx ~= 6
-        set(gca,'XTick',[])
-    end
-    if idx == 1
-        title('Signal','FontW','Normal')
-    end    
-    set(gca,'FontSize',20,'FontName','Arial')
+    figure,clf
+    subplot(3,3,1:3)
+    ph   = plot(tseg,music_segments{idx}(:,1),'color',colors(1,:));
+    set(gca,'ylim',[-.8 .8],'xlim',[0.9 28.90],'box','off','XTick',[],'FontSize',12)
+    ylabel('Norm. amplitude')
+    subplot(3,3,4:6)
+    imagesc(linspace(1,30,length(music_v)),freqs,music_v_all(:,:,idx)')
+    set(gca,'YDir','normal')
+    set(gca,'clim',[0 1.5], 'xlim', [0.9 28.90],'box','off','XTick',[],'FontSize',12)
+    ylabel('Frequency (Hz)')
+    saveas(gca,[Fig_Dir,filesep,'Acoustics_music_segment' num2str(idx) '.tiff'])
+    subplot(3,3,7:9)
+    envh = plot(linspace(0,30,length(envelope_music)),envelope_music(idx,:),'k');
+    set(gca,'ylim',[-.8 .8],'xlim',[0.9 28.90],'box','off','FontSize',12)
+    ylabel('Norm. amplitude'); xlabel('Time (s)')
+    saveas(gca,[Fig_Dir,filesep,'Acoustics_music_segment' num2str(idx), '.tiff'])
 end
-xlabel('Time (s)')
 
 for idx=1:length(speech_segments)
-    figure(4)
-    subplot(6,2,(idx*2)-1)
-    ph = plot(tseg,speech_segments{idx}(:,1),'color',colors(2,:));
-    ylim([-1 1]); xlim([0 28.90]);
-    box off
-    if idx ~= 6
-        set(gca,'XTick',[])
-    end
-    if idx == 1
-        title('Signal','FontW','Normal')
-    end
-    set(gca,'FontSize',20,'FontName','Arial')
+    figure,clf
+%     subplot(6,2,(idx*2)-1)
+    subplot(3,3,1:3)
+    ph   = plot(tseg,speech_segments{idx}(:,1),'color',colors(2,:));
+    set(gca,'ylim',[-.8 .8],'xlim',[0.9 28.90],'box','off','XTick',[],'FontSize',12)
+    ylabel('Norm. amplitude')
+    subplot(3,3,4:6)
+    imagesc(linspace(1,30,length(speech_v)),freqs,speech_v_all(:,:,idx)')
+    set(gca,'YDir','normal')
+    set(gca,'clim',[0 1.5], 'xlim',[0.9 28.90],'XTick',[],'FontSize',12)
+    ylabel('Frequency (Hz)')
+    subplot(3,3,7:9)
+    envh = plot(linspace(0,30,length(envelope_speech)),envelope_speech(idx,:),'k');
+    set(gca,'ylim',[-.8 .8],'xlim',[0.9 28.90],'box','off','FontSize',12)
+    ylabel('Norm. amplitude'); xlabel('Time (s)')  
+    saveas(gca,[Fig_Dir,filesep,'Acoustics_speech_segment' num2str(idx), '.tiff'])
 end
-xlabel('Time (s)')
-%% 
-% now plot the cochlear envelopes
-for idx=1:length(music_segments)-1
-    figure(3)
-    subplot(6,2,idx*2)
-    ph = plot(linspace(0,30,length(envelope_music)),envelope_music(idx,:),'color',colors(1,:));
-    ylim([-1 1]); xlim([0 28.90]);
-    box off
-    if idx ~= 6
-        set(gca,'XTick',[])
-    end
-    if idx == 1
-        title('Envelope','FontW','Normal')
-    end
-    set(gca,'FontSize',20,'FontName','Arial')
-end
-xlabel('Time (s)')
-
-% now plot the cochlear envelopes
-for idx=1:length(speech_segments)
-    figure(4)
-    subplot(6,2,idx*2)
-    ph = plot(linspace(0,30,length(envelope_speech)),envelope_speech(idx,:),'color',colors(2,:));
-    ylim([-1 1]); xlim([0 28.90]);
-    box off
-    if idx ~= 6
-        set(gca,'XTick',[])
-    end
-    if idx == 1
-        title('Envelope','FontW','Normal')
-    end
-    set(gca,'FontSize',20,'FontName','Arial')
-end
-xlabel('Time (s)')
-
-figure(3)
-sh = suptitle('Music');
-set(sh,'FontSize',24)
-figure(4)
-suptitle('Speech')
-sh = suptitle('Speech');
-set(sh,'FontSize',24)
